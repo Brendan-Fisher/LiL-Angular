623a6eed2649bca4f8876a796ba21a16eb23f5bb
Brendan-Fisher <fisherb1@ufl.edu>
2022-06-09 16:31:19 -0400

    Learning Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: [
+        `
+            .list-group-item:first-child {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                border-top: 0;
+            }
+        `,
+    ],
 })
-export class AppComponent {
-  title = 'learn-angular';
+export class AppComponent implements OnInit {
+    query: string;
+    artists: any[];
+    currentArtist: any;
+
+    showArtist(artist: any) {
+        this.query = artist.name;
+        artist.highlight = !artist.highlight;
+        this.currentArtist = artist;
+    }
+
+    constructor(private http: HttpClient) {
+        this.query = '';
+        this.artists = [];
+    }
+
+    ngOnInit(): void {
+        this.http.get<any>('../assets/data.json').subscribe((data) => {
+            this.artists = data;
+        });
+    }
 }
